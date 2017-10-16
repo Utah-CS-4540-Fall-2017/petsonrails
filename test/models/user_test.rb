516fdf32a9777cadd0152fa8c83ed869bf668de8
David L Bean <david.l.bean@mac.com>
2017-10-16 16:08:11 -0600

    user model tests written

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,4 +32,59 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def required_attribute_missing(object, attr_name)
+    # try saving the object which will set the errors attrib
+    object.save
+    object.errors.keys.include?(attr_name) &&
+      object.errors[attr_name].include?("can't be blank")
+  end
+
+  def verify_existence_of(object, attr_name)
+    # try to save the object to set the errors attribute
+    object.save
+    if object.errors.keys.include?(attr_name) &&
+       object.errors[attr_name].include?("can't be blank")
+      return false
+    else
+      return true
+    end
+  end
+
+  test 'first name required' do
+    jack = users(:pirate)
+    # ensure the object is ok right now
+    assert jack.save
+    jack.first_name = ''
+    # the save should fail now
+    assert_not jack.save
+    # and verify the error is getting set
+    assert required_attribute_missing(jack, :first_name)
+  end
+
+  test 'last name required' do
+    jack = users(:pirate)
+    # ensure the object is ok right now
+    assert jack.save
+    jack.last_name = ''
+    # the save should fail now
+    assert_not jack.save
+    # and verify the error is getting set
+    assert required_attribute_missing(jack, :last_name)
+  end
+
+  test 'password is required on new records' do
+    u = User.new
+    refute verify_existence_of(u, :password)
+  end
+
+  test 'password is NOT required on existing records' do
+    jack = users(:pirate)
+    assert jack.save
+  end
+
+  test 'full_name returns concatenated first and last names' do
+    kirk = users(:captain)
+    assert kirk.full_name == 'James Kirk'
+  end
 end
